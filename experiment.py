d8da40bac45e6e3e0162577c2f45c02436e53ba8
Jack Hill <jackhill2104@gmail.com>
2024-09-10 00:02:17 +0930

    Made some changes to loops (looping over file lines, looping through traces) to make them more pythonic

--- experiment.py
+++ experiment.py
@@ -9,7 +9,8 @@ from randmmu import RandMMU
 
 PAGE_OFFSET = 12  # page is 2^12 = 4KB
 
-class Trace():
+
+class Trace:
     def __init__(self, name, traces):
         self.name = name
         self.traces = traces
@@ -17,49 +18,52 @@ class Trace():
         self.rand_results = []
         self.lru_results = []
         self.clock_results = []
-        
+
     def NumMemoryAccesses(self):
         return len(self.traces)
-    
+
     def UniqueFrames(self):
         return len(np.unique(self.traces[0:, 0]))
-       
-file_names = [ 'bzip', 'sixpack', 'swim', 'gcc' ]
-traces = []   
+
+
+file_names = ["bzip", "sixpack", "swim", "gcc"]
+traces = []
 
 for file in file_names:
-    curr_traces = []   
-    
-    with gzip.open(file + '.trace.gz', 'rt') as file:
-        for trace_line in file.read().split("\n"):
+    curr_traces = []
+
+    with gzip.open(file + ".trace.gz", "rt") as file:
+        for trace_line in file:
             curr_trace = trace_line.split()
-            
+
             if len(curr_trace) == 0:
                 continue
-            
+
             logical_address = int(curr_trace[0], 16)
             curr_trace[0] = logical_address >> PAGE_OFFSET
-            
+
             curr_traces.append(curr_trace)
-              
+
     traces.append(Trace(file, np.array(curr_traces)))
 
 
-for x in range(1,2):
-    print(str(x * 10) + "% of unique frames" )
+for x in range(1, 2):
+    print(str(x * 10) + "% of unique frames")
 
     for i in range(0, 4):
         traces[i].increments.append(x * 0.1)
-        
+
         frames = int(math.ceil(traces[i].UniqueFrames() * x * 0.1))
         rand = RandMMU(frames)
         lru = LruMMU(frames)
         clock = ClockMMU(frames)
-        
-        for j in range(0, traces[i].NumMemoryAccesses()):
-            if (j % 10000 == 0):
-                print("\rtrace : " + file_names[i] + " mem. address : " + str(j), end='')
-            trace_cmd = traces[i].traces[j]
+
+        for j, trace_cmd in enumerate(traces[i].traces):
+            if j % 10000 == 0:
+                print(
+                    "\rtrace : " + file_names[i] + " mem. address : " + str(j), end=""
+                )
+
             if trace_cmd[1] == "R":
                 rand.read_memory(trace_cmd[0])
                 lru.read_memory(trace_cmd[0])
@@ -72,13 +76,13 @@ for x in range(1,2):
         rand_fault_rate = rand.get_total_page_faults() / traces[i].NumMemoryAccesses()
         lru_fault_rate = lru.get_total_page_faults() / traces[i].NumMemoryAccesses()
         clock_fault_rate = clock.get_total_page_faults() / traces[i].NumMemoryAccesses()
-        
+
         traces[i].rand_results.append(rand_fault_rate)
         traces[i].lru_results.append(lru_fault_rate)
         traces[i].clock_results.append(clock_fault_rate)
-        
+
         print()
-                
+
 for i in range(0, 4):
     plt.figure(i)
 
@@ -86,3 +90,4 @@ for i in range(0, 4):
     plt.plot(traces[i].increments, traces[i].lru_results, label="lru")
     plt.plot(traces[i].increments, traces[i].clock_results, label="clock")
 
+    plt.show()
